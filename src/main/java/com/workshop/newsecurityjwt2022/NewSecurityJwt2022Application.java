efde06f0d81f81d8618b3c1d80c8875e92447651
NaderAyed <nader.ayed@esprit.tn>
2022-12-07 21:34:22 +0100

    initial step

---
+++ src/main/java/com/workshop/newsecurityjwt2022/NewSecurityJwt2022Application.java
@@ -0,0 +1,13 @@
+package com.workshop.newsecurityjwt2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSecurityJwt2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSecurityJwt2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/workshop/newsecurityjwt2022/NewSecurityJwt2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.newsecurityjwt2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSecurityJwt2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
